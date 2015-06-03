00fa29e2699f6b6f5d7c7d5d59f3b45151f06e43
russc <chris.russ@franklin.edu>
2015-06-03 16:57:04 -0400

    refactored execute. refactored style.

--- wombat.js
+++ wombat.js
@@ -7,20 +7,19 @@ Wombat.prototype.createWombox = function(){
   wombox.style.zIndex = '10000';
   wombox.style.position = 'fixed';
   wombox.style.top = '0';
-  wombox.style.width = '30%';
+  wombox.style.right = '0';
+  // wombox.style.width = '30%';
+  wombox.style.margin = '5px';
+  wombox.style.padding = '5px';
   wombox.style.borderRadius = '5px';
   wombox.style.boxShadow = '1px 1px 5px';
   wombox.style.backgroundColor = 'white';
   wombox.style.textAlign = 'center';
   wombox.style.overflow = 'auto';
+  wombox.style.fontFamily = 'Calibri';
+  wombox.style.fontSize = '12px';
   wombox.bottom = '1em';
   wombox.right = '1em';
-  wombox.draggable = true;
-  wombox.ondragend = function(event){
-    wombox = event.target;
-    wombox.style.top = event.y - wombox.clientHeight+ 'px';
-    wombox.style.left = event.x + 'px';
-  };
   wombox.appendChild(this.createInput());
   wombox.appendChild(this.createTextArea());
   return wombox;
@@ -28,14 +27,14 @@ Wombat.prototype.createWombox = function(){
 
 Wombat.prototype.createInput = function(){
   var input = document.createElement('input');
-  input.style.width = '95%';
-  input.style.margin = '5px auto';
-  input.style.marginTop = '10px';
-  input.style.marginBottom = '10px';
+  input.style.fontFamily = 'Calibri';
+  input.style.fontSize = '14px';
+  input.style.width = '300px';
+  input.style.margin = '0 auto';
   input.style.padding = '5px';
   input.style.borderRadius = '7px';
+  input.style.boxShadow = '0 0 1px inset';
   input.style.resize = 'both';
-  input.boxShadow = '1px 1px 5px';
   input.onkeydown = this.inputHandler;
   input.placeholder = 'waiting for your command...';
   return input;
@@ -43,13 +42,15 @@ Wombat.prototype.createInput = function(){
 
 Wombat.prototype.createTextArea = function(){
   var textarea = document.createElement('textarea');
-  textarea.style.width = '95%';
-  textarea.style.margin = '5px auto';
-  textarea.style.marginTop = '0';
-  textarea.style.marginBottom = '10px';
+  textarea.style.fontFamily = 'Calibri';
+  textarea.style.fontSize = '14px';
+  textarea.style.width = '300px';
+  textarea.style.height = '300px';
+  textarea.style.margin = '0 auto';
+  textarea.style.marginTop = '5px';
   textarea.style.padding = '5px';
   textarea.style.borderRadius = '7px';
-  textarea.style.boxShadow = '1px 1px 2px inset';
+  textarea.style.boxShadow = '0 0 1px inset';
   textarea.style.resize = 'vertical';
   textarea.style.display = 'none';
   return textarea;
@@ -67,13 +68,21 @@ Wombat.prototype.inputHandler = function(event){
       textarea.value = '';
     } else {
       var command = localStorage.getItem(input.value);
-      try {
-        input.placeholder = eval(command);
-      } catch(error){
-        input.placeholder = error.message;
+      if (!command) {
+        input.placeholder = 'name your command.';
+        textarea.value = localStorage.getItem(input.value);
+        textarea.style.display = 'block';
+        textarea.placeholder = 'define your command.'; 
+      } else {lo
+        try {
+          input.placeholder = eval(command);
+        } catch(error){
+          input.placeholder = error.message;
+        }
+        input.value = '';
       }
+
     }
-    input.value = '';
   } else if (event.keyCode == 17){
     if (textarea.style.display == 'none'){
       input.placeholder = 'name your command.';
